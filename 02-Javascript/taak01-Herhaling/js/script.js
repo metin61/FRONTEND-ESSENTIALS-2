04ce06be4ba9189dd57dc975e8a5da16aeb41ba3
metin celik <metin.celik@student.rocva.nl>
2021-03-04 13:48:48 +0100

    fe2

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,20 @@
-let strNaam;
-let boolNaamKlopt;
+
+
+strNaam = prompt("Hoe heet je?");
+boolNaamKlopt = confirm("Heet je echt " + strNaam +"?"); 
+
+if(boolNaamKlopt == true) {
+    alert("Welkom " + strNaam );
+}
+
+else if(boolNaamKlopt==false){
+    prompt("nog een keer hoe heet je?");
+    
+}else if (boolNaamKlopt==true){
+    alert("heet je" + strNaam);
+    
+}
+
+
+console.log(strNaam)
+
--- 02-Javascript/taak03-Functions-Return/js/script.js
+++ 02-Javascript/taak03-Functions-Return/js/script.js
@@ -1,5 +1,10 @@
-function geefRandomGetalTerug(){                
-    
+
+
+function geefRandomGetalTerug(){                    //randomGetal =
+    let randomGetal = Math.random();                //0.54654983435
+    randomGetal = randomGetel *100 + 1;             //54.654983435
+    randomGetal = Math.floor(randomGetal);          //54
+    return randomGetal;
 }
 
-console.log(geefRandomGetalTerug());    
+console.log(geefRandomGetalTerug());                //het getal 54 wordt getoond in de console
